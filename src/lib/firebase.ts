682422926f1d28b639ed8dde51031a73b6d10af1
Jonatas <admin@007swiper.com>
2026-06-05 04:52:13 -0300

    Fix: Chaves reais do Firebase

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,13 +3,13 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyAF948O6dAwkSvPJSVHg1FYMm9vE1RneI4',
-  authDomain: 'swiper-db-21c6f.firebaseapp.com',
-  projectId: 'swiper-db-21c6f',
-  storageBucket: 'swiper-db-21c6f.firebasestorage.app',
-  messagingSenderId: '235296129520',
-  appId: '1:235296129520:web:612a9c5444064ce5b11d35',
-  measurementId: 'G-SGQY0W9CWC'
+  apiKey: "AIzaSyAF94806dAwkSvPJSVHglfYMm9vE1Rnei4",
+  authDomain: "swiper-db-21c6f.firebaseapp.com",
+  projectId: "swiper-db-21c6f",
+  storageBucket: "swiper-db-21c6f.firebasestorage.app",
+  messagingSenderId: "235296129520",
+  appId: "1:235296129520:web:612a9c5444064ce5b11d35",
+  measurementId: "G-SGQY0W9CWC"
 };
 
 const app = initializeApp(firebaseConfig);
